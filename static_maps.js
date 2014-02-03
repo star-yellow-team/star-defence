c522752a38fe552d1debfdb282776079d76bd282
Andrei Stefanescu <andysstef@yahoo.com>
2014-02-03 17:51:24 +0200

    First commit

---
+++ static_maps.js
@@ -0,0 +1,23 @@
+// x = line
+// y = coloana
+// 0 = liber
+// 1 = traseu monstru
+
+function Map(sx, sy, bx, by, m)
+{
+	This.spawnx = sx;
+	This.spawny = sy;
+	This.basex = bx;
+	This.basey = by;
+	This.matrix = m;
+}
+
+Map.prototype.addElement = function(x, y, element)
+{
+	matrix[x][y] = element;
+}
+
+Map.prototype.removeElement = function(x, y)
+{
+	matrix[x][y] = 0;
+}
