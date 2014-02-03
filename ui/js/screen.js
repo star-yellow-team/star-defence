20b495d1e06cfdc2fd76374771a6379404e3c40e
Andrei Stefanescu <andysstef@yahoo.com>
2014-02-03 23:18:03 +0200

    Added resize handler

--- ui/js/screen.js
+++ ui/js/screen.js
@@ -1,24 +1,52 @@
-// JavaScript Document
+/**
+ *@author Adrian, Andrei
+ * Defineste functia size() care va redimensiona body-ul documentului HTML la prima incarcare si de fiecare data cand se modifica 
+ * lungimea si inaltimea documentului.
+ */
 
-var screenWidth = $(window).width();
+
+var screenWidth  = $(window).width();
 var screenHeight = $(window).height();
-var screenSize
-var boxSize
+var screenSize   = 0
+var boxSize      = 0
 
-if (screenWidth < screenHeight) {
-	screenSize = screenWidth;
-} else {
-	screenSize = screenHeight;
+
+
+function sizeScreen() {
+    // preia dimensiunile documentului
+    screenWidth = $(window).width();
+    screenHeight = $(window).height();
+   
+    // o selectam pe cea mai mica
+    if (screenWidth < screenHeight) {
+            screenSize = screenWidth;
+    } else {
+            screenSize = screenHeight;
+    }
+
+    // setam dimesniunea unei celule
+    boxSize = Math.floor(screenSize / 20);
+
+    screenSize = 20 * boxSize;
+    
+    // redimensionam
+    $("body").css("height",screenSize);
+    $("body").css("width",screenSize);
+
+    // redimensionam
+    $(".ghostImage").css("height",boxSize);
+    $(".ghostImage").css("width",boxSize);
+
+    var bodyLeftMargin = parseInt($("body").css("marginLeft")) + 8;
+    
 }
 
-boxSize = Math.floor(screenSize / 20);
+// o apelam pentru prima oara
+sizeScreen();
 
-screenSize = 20 * boxSize;
 
-$("body").css("height",screenSize);
-$("body").css("width",screenSize);
+// o setam ca handler pentru evenimentul de resize
+$(window).resize(function() {
+    size()
+});
 
-$(".ghostImage").css("height",boxSize);
-$(".ghostImage").css("width",boxSize);
-
-var bodyLeftMargin = parseInt($("body").css("marginLeft")) + 8;
