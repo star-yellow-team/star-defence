a0bacbb85ae1efea4a9c1debd3989ced1fd65856
Adrian Claudiu Covaci <adrianclaudiucovaci@gmail.com>
2014-02-03 22:13:34 +0200

    UI Turret Snap
    Will upload documentation maine

--- UI Test/js/buttons.js
+++ UI Test/js/buttons.js
@@ -1,5 +1,7 @@
 // JavaScript Document
 var blueHover = 0;
+var boxX;
+var boxY;
 
 $(document).ready(function() {
 	$("#blue").hide();
@@ -10,8 +12,20 @@ var currentMousePos = { x: -1, y: -1 };
 $(document).mousemove(function(event) {
 	if (blueHover == 1) {
 		$("#blue").show();
-        $("#blue").css("left",event.pageX - 50);
-       	$("#blue").css("top",event.pageY - 50);
+		if (event.pageX < bodyLeftMargin) {
+			boxX = bodyLeftMargin;
+		} else if (event.pageX > bodyLeftMargin + screenSize) {
+			boxX = bodyLeftMargin + screenSize - boxSize;
+		} else {
+			boxX = bodyLeftMargin + Math.round((event.pageX - bodyLeftMargin - boxSize / 2) / boxSize) * boxSize;
+		}
+		if (event.pageY > screenSize) {
+			boxY = screenSize - boxSize;
+		} else {
+			boxY = Math.round((event.pageY - boxSize / 2) / boxSize) * boxSize;
+		}
+        $("#blue").css("left",boxX);
+       	$("#blue").css("top",boxY);
 	}
 });
 	
@@ -25,7 +39,7 @@ function unstick() {
 }
 
 function second() {
-	alert("You've clicked the second button!")
+	alert(p)
 }
 
 function third() {
--- UI Test/js/screen.js
+++ UI Test/js/screen.js
@@ -1,1 +1,24 @@
 // JavaScript Document
+
+var screenWidth = $(window).width();
+var screenHeight = $(window).height();
+var screenSize
+var boxSize
+
+if (screenWidth < screenHeight) {
+	screenSize = screenWidth - 110;
+} else {
+	screenSize = screenHeight - 110;
+}
+
+boxSize = Math.floor(screenSize / 20);
+
+screenSize = 20 * boxSize;
+
+$("body").css("height",screenSize);
+$("body").css("width",screenSize);
+
+$(".ghostImage").css("height",boxSize);
+$(".ghostImage").css("width",boxSize);
+
+var bodyLeftMargin = parseInt($("body").css("marginLeft"));
