7927d5514b623f7f781f459c307b131bf3032daf
Adrian Claudiu Covaci <adrianclaudiucovaci@gmail.com>
2014-02-03 20:13:50 +0200

    Added test functions

--- UI Test/js/buttons.js
+++ UI Test/js/buttons.js
@@ -1,1 +1,13 @@
 // JavaScript Document
+
+function first() {
+	alert("You've clicked the first button!")
+}
+
+function second() {
+	alert("You've clicked the second button!")
+}
+
+function third() {
+	alert("You've clicked the third button!")
+}
