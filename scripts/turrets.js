2b36e822d47f288050e49a8963a3aaf299f51537
Ionut <ionut_SC94@yahoo.com>
2014-02-08 22:48:42 +0200

    in progress

--- scripts/turrets.js
+++ scripts/turrets.js
@@ -3,26 +3,130 @@
 * Definirea Turret-ului
 * daca lipseste ceva va rog spuneti
 */
-function turret(name, damage, range, attack_speed, projectile_type, damage_type, level, price, skin, detection, kills, requirement, footprint, portrait, description, icon)
-{
-this.name=name;
-this.damage=damage;
-this.range=range;
-this.attack_speed=attack_speed;
-this.projectile_type=projectile_type;
-this.damage_type=damage_type;
-this.level=level;
-this.price=price;
-this.detection=detection;
-this.kills=kills;
-this.skin=skin;
-this.requirement=requirement;
-this.icon=icon;
-this.footprint=footprint;
-this.description=description
-//optional
-this.portrait=portrait
 
+//Tipuri de turete
+var MACHINEGUN_TURRET = {
+	id:	0,
+	damage:	6,
+	range:	5,
+	attackSpeed:	0.8,
+	damageType:	"Single",
+	upgradeLevel:	0,
+	price:	10,
+	kills:	0,
+	requirement:	"None",
+	description:	"Fast attacking turret"
+}
+
+var SLOW_TURRET = {
+	id:	1,
+	range:	5,
+	upgradeLevel:	0,
+	price:	20,
+	requirement:	"None",
+	description:	"Slows enemies in their path",
+	slow:	"Yes"
+}
+
+var PLASMA_TURRET = {
+	id:	2,
+	damage:	20,
+	range:	6,
+	attackSpeed:	1.5,
+	damageType:	"Splash",
+	upgradeLevel:	0,
+	price:	40,
+	kills:	0,
+	requirement:	"None",
+	description:	"Strong turret against swarms of small units",
+}
+
+var LASER_TURRET = {
+	id:	3,
+	damage:	2,
+	range:	5,
+	attackSpeed:	0.2,
+	damageType:	"Single",
+	upgradeLevel:	0,
+	price:	50,
+	kills:	0,
+	requirement:	"Pass level 6",
+	description:	"Fires laser beams at enemy targets"
+}
+
+var DETECTOR_TURRET = {
+	id:	4,
+	range:	7,
+	upgradeLevel:	0,
+	price:	20,
+	detection: "Yes",
+	requirement:	"Pass level 8",
+	description:	"Reveals invisible enemies within range"
+}
+
+function turret(type)
+{
+	this.type=type;
+	switch(type)	{
+		case MACHINEGUN_TURRET.id:
+			this.damage=MACHINEGUN_TURRET.damage;
+			this.range=MACHINEGUN_TURRET.range;
+			this.attackSpeed=MACHINEGUN_TURRET.attackSpeed;
+			this.damageType=MACHINEGUN_TURRET.damageType;
+			this.upgradeLevel=MACHINEGUN_TURRET.upgradeLevel;
+			this.price=MACHINEGUN_TURRET.price;
+			this.kills=MACHINEGUN_TURRET.kills;
+			this.requirement=MACHINEGUN_TURRET.requirement;
+			this.description=MACHINEGUN_TURRET.description;
+			break;
+		
+		
+		case SLOW_TURRET.id:
+			this.range=SLOW_TURRET.range;
+			this.upgradeLevel=SLOW_TURRET.upgradeLevel;
+			this.price=SLOW_TURRET.price;
+			this.requirement=SLOW_TURRET.requirement;
+			this.description=SLOW_TURRET.description;
+			this.slow=SLOW_TURRET.slow;
+			break;
+
+		case PLASMA_TURRET.id:
+			this.damage=PLASMA_TURRET.damage;
+			this.range=PLASMA_TURRET.range;
+			this.attackSpeed=PLASMA_TURRET.attackSpeed;
+			this.damageType=PLASMA_TURRET.damageType;
+			this.upgradeLevel=PLASMA_TURRET.upgradeLevel;
+			this.price=PLASMA_TURRET.price;
+			this.kills=PLASMA_TURRET.kills;
+			this.requirement=PLASMA_TURRET.requirement;
+			this.description=PLASMA_TURRET.description;
+			break;
+
+		case LASER_TURRET.id:
+			this.damage=LASER_TURRET.damage;
+			this.range=LASER_TURRET.range;
+			this.attackSpeed=LASER_TURRET.attackSpeed;
+			this.damageType=LASER_TURRET.damageType;
+			this.upgradeLevel=LASER_TURRET.upgradeLevel;
+			this.price=LASER_TURRET.price;
+			this.kills=LASER_TURRET.kills;
+			this.requirement=LASER_TURRET.requirement;
+			this.description=LASER_TURRET.description;
+			break;
+
+		case DETECTOR_TURRET.id:
+			this.range=DETECTOR_TURRET.range;
+			this.upgradeLevel=DETECTOR_TURRET.upgradeLevel;
+			this.price=DETECTOR_TURRET.price;
+			this.detection=DETECTOR_TURRET.detection;
+			this.requirement=DETECTOR_TURRET.requirement;
+			this.description=DETECTOR_TURRET.description;
+			break;
+
+		default:
+			console.log("Invalid turret type!");
+			break;
+}
 // Aici nu stiu inca cum vom face sa atace turnul.
 function attack()
 {
@@ -38,8 +142,7 @@ function detect_enemy()
 function upgrade()
 {
 money=money-price;
-this.skin="newskin.jpg"
+upgradeLevel++;
 }
 
 }
-
