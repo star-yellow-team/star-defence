e5c737818d701d14f689aca93072e345054ec998
Romaniuc Radu <radu.romaniuc@yahoo.com>
2014-02-20 20:29:32 +0200

    Update globals.js

--- scripts/globals.js
+++ scripts/globals.js
@@ -20,3 +20,9 @@ var totalScore      =  0; //Se refera la scorul total per batalie. NU total per 
 var totalCredits    =  0; //Se refera la banii pe care ii are jucatorul per batalie.
 
 var turrets         = [];
+
+
+//Achievements variables. Ca sa fie mai organizat.
+var allTime_Enemies_Defeated = 0;
+//IMPORTANT! Acum ne dam seama ca trebuie sa pastram undeva, in memorie, achievementurile, ca altfel, la pornirea
+//jocului, se initializeaza toate cu 0. So va fi ceva gen: var allTime_X = function GetFromMemory.
