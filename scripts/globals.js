98613ad57d0f65c6db5fbd5152f7db3f1c59a8f2
Romaniuc Radu <radu.romaniuc@yahoo.com>
2014-02-23 17:57:58 +0200

    Achievements Integrated

--- scripts/globals.js
+++ scripts/globals.js
@@ -16,13 +16,29 @@ var nrmax_path      =  0;
 var curentRound     = -1;
 var boxSize         =  0;
 var userScore       =  0;
-var totalScore      =  0; //Se refera la scorul total per batalie. NU total per tot jocul.
-var totalCredits    =  0; //Se refera la banii pe care ii are jucatorul per batalie.
 
 var turrets         = [];
 
 
-//Achievements variables. Ca sa fie mai organizat.
-var allTime_Enemies_Defeated = 0;
-//IMPORTANT! Acum ne dam seama ca trebuie sa pastram undeva, in memorie, achievementurile, ca altfel, la pornirea
-//jocului, se initializeaza toate cu 0. So va fi ceva gen: var allTime_X = function GetFromMemory.
+//Achievements
+var time_passed_perBattle = 0; var totalCredits = 0; var totalScore = 0; var fun_activated = true; // TIMP, CREDITS, SCOR, FUN
+
+var enemies_defeated_perBattle = 0;	 //ENEMIES
+var first_enemy_defeated = false;
+var first_10_enemies_defeated = false;
+var first_50_enemies_defeated = false;
+var first_100_enemies_defeated = false;
+var first_1000_enemies_defeated = false;
+
+var turrets_placed_perBattle =0;  //TURRETS
+var first_turret_placed = false;
+var first_10_turrets_placed = false;
+var first_20_turrets_placed = false;
+
+var waves_won_perBattle = 0; //WAVES
+var first_wave_won = false;
+var first_10_waves_won = false;
+var first_50_waves_won = false;
+var first_100_waves_won = false;
+
+var first_100_credits = false;
