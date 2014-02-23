985f5c944296dd218542def2f085dcd06282bf5f
Andrei Stefanescu <andysstef@yahoo.com>
2014-02-23 21:30:36 +0200

    Some database stuff

--- backend/db.py
+++ backend/db.py
@@ -4,7 +4,26 @@ import sqlite3
 # TODO --> implementeaza facilitate
 # de logare
 def sign_in_user(name, password):
-    pass
+    conn    = sqlite3.connect('db.sql')
+    cursor  = None
+    user    = None
+
+    try:
+        cursor  = conn.execute("SELECT * FROM USERS WHERE name = ? AND PASSWORD = ?", (name, password))
+        user    = cursor.fetchone()
+    except sqlite3.Error as e:
+        print e
+    finally:
+        if conn is not None:
+            if cursor is not None:
+                cursor.close()
+            conn.close()
+
+        return user
+        
+
+
+    # pass
 
 # TODO --> implementeaza facitilitate
 # de signup
