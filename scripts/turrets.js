351b46f91782966c4831252932857f2d1a200a41
Stefan <star.yellowteam@gmail.com>
2014-02-27 23:05:29 +0200

    merge

--- scripts/turrets.js
+++ scripts/turrets.js
@@ -29,6 +29,7 @@ var MACHINEGUN_TURRET = {
 	contor:	0,
 	x:	0,
 	y:	0,
+	level: 0,
     sprite:         'spirte.png',
     spriteSize:    5
 }
@@ -46,6 +47,7 @@ var SLOW_TURRET = {
 	amount:	2,
 	price:	20,
 	kills:	0,
+	level: 0,
 	requirement:	"None",
 	description:	"Slows enemies in their path",
 	isAttacking:	false,
@@ -71,6 +73,7 @@ var PLASMA_TURRET = {
 	amount:	0,
 	price:	35,
 	kills:	0,
+	level: 0,
 	requirement:	"None",
 	description:	"Strong turret against swarms of small units",
 	isAttacking:	false,
@@ -149,12 +152,14 @@ function Turret(type)
 			this.description=MACHINEGUN_TURRET.description;
 			this.isAttacking=MACHINEGUN_TURRET.isAttacking;
 			this.contor=MACHINEGUN_TURRET.contor;
+			this.level=MACHINEGUN_TURRET.level;
 	                this.sprite     = MACHINEGUN_TURRET.sprite;
                         this.spriteSize = MACHINEGUN_TURRET.spriteSize;
             		break;
 		
 		
 		case SLOW_TURRET.id:
+			this.level=SLOW_TURRET.level;
 			this.damage=SLOW_TURRET.damage;
 			this.range=SLOW_TURRET.range;
 			this.attackSpeed=SLOW_TURRET.attackSpeed;
@@ -173,6 +178,7 @@ function Turret(type)
 
 
 		case PLASMA_TURRET.id:
+			this.level=PLASMA_TURRET.level;
 			this.damage=PLASMA_TURRET.damage;
 			this.range=PLASMA_TURRET.range;
 			this.attackSpeed=PLASMA_TURRET.attackSpeed;
@@ -189,6 +195,7 @@ function Turret(type)
 			break;
 
 		case LASER_TURRET.id:
+			this.level=LASER_TURRET.level;
 			this.damage=LASER_TURRET.damage;
 			this.range=LASER_TURRET.range;
 			this.attackSpeed=LASER_TURRET.attackSpeed;
@@ -205,6 +212,7 @@ function Turret(type)
 			break;
 
 		case DETECTOR_TURRET.id:
+			this.level=DETECTOR_TURRET.level;
 			this.damage=DETECTOR_TURRET.damage;
 			this.range=DETECTOR_TURRET.range;
 			this.attackSpeed=DETECTOR_TURRET.attackSpeed;
