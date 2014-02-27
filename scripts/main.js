47510755614ee08e6e667f60f0742763335fb15f
Adrian Claudiu Covaci <adrianclaudiucovaci@gmail.com>
2014-02-27 19:59:06 +0200

    aa

--- scripts/main.js
+++ scripts/main.js
@@ -195,5 +195,14 @@ function main() {
         gameLoop();
 
 }
+/**
+ $.notify.defaults({autoHide:false});
+
++$.notify("Random message");
+
++$.notify("Random message","info");
+
++$.notify("Random message","warn");*
+ * */
 
 
