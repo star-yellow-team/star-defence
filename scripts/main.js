1152d48240772a93dded3cf89f94f704c7a2cbeb
Adrian Claudiu Covaci <adrianclaudiucovaci@gmail.com>
2014-02-27 19:54:11 +0200

    Rendăm staf

--- scripts/main.js
+++ scripts/main.js
@@ -195,7 +195,6 @@ function main() {
         gameLoop();
 
 }
-$.notify.defaults({autoHide:false});
 $.notify("Random message");
 $.notify("Random message","info");
 $.notify("Random message","warn");
