92f3844bfb7a07a16a2fab90926c49890582c41f
Adrian Claudiu Covaci <adrianclaudiucovaci@gmail.com>
2014-03-01 13:52:55 +0200

    Pasue Menu finetuning

--- scripts/screen.js
+++ scripts/screen.js
@@ -129,12 +129,14 @@ function sizeScreen() {
 	$("#paused").css("top",screenHeight / 2 - 6 * boxSize);
 	$("#play").css("top",screenHeight / 2 - 2 * boxSize);
 	$("#restart").css("top",screenHeight / 2 - 2 * boxSize);
-	$("#play").css("left",screenWidth / 2 - 5 * boxSize);
+	$("#play").css("left",screenWidth / 2 - 5.5 * boxSize);
 	$("#restart").css("left",screenWidth / 2 + 0.5 * boxSize);
 	$("#play").css("width",5 * boxSize);
 	$("#play").css("height",5 * boxSize);
 	$("#restart").css("width",5 * boxSize);
 	$("#restart").css("height",5 * boxSize);
+
+	$("#paused").css("left",(screenWidth - $("#paused").width()) / 2);
 }
 
 // o apelam pentru prima oara
