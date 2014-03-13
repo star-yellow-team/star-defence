e43a94eb69ac1c4732a2531ecb3176edf55a8045
Andrei Stefanescu <andysstef@yahoo.com>
2014-03-14 00:36:13 +0200

    Added finished game message

--- scripts/globals.js
+++ scripts/globals.js
@@ -264,3 +264,15 @@ function gameOver() {
 
     return false;
 }
+
+// http://www.phpied.com/sleep-in-javascript/
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
+
