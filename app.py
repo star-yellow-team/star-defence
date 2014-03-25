462006c9dc1a6febe629d87c7fe7a99a4730f034
AndreiRO <andysstef@yahoo.com>
2014-03-25 13:01:11 +0200

    More stuff for heroku

--- app.py
+++ app.py
@@ -6,6 +6,3 @@ app = flask.Flask(__name__, template_folder=".")
 def game_page():
     return flask.render_template('game.html')
 
-
-if __name__ == '__main__':
-    app.run(port=8080, debug = True)
